4d9aceb830d8cf2a19c0825666d7a9a0ea067269
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-17 08:40:38 -0300

    new transaction modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
@@ -5,12 +6,19 @@ export function Header() {
     <header className="bg-neutral-900 pt-10 pb-28">
       <div className="w-full max-w-[1120px] mx-auto px-6 flex justify-between items-center`">
         <img src={igniteLogo} alt="" />
-        <button
-          className="h-[50px] border-none bg-emerald-600 text-white font-bold px-5 rounded-md cursor-pointer hover:bg-emerald-700 transition"
-          type="button"
-        >
-          Nova transação
-        </button>
+        <Dialog.Root>
+          <Dialog.Trigger className="h-[50px] border-none bg-emerald-600 text-white font-bold px-5 rounded-md cursor-pointer hover:bg-emerald-700 transition">
+            <span>Nova transação</span>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
     </header>
   )
