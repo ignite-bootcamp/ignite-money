f75ac3010b1f9aef82c1c9441b2f996442cc9d9c
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-17 08:20:23 -0300

    transactions table

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { Header } from '../components/Header'
 import { Summary } from '../components/Summary'
 
@@ -6,6 +7,33 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <main className="mx-auto max-w-[1120px] w-full px-6 mt-16">
+        <table className="w-full mt-6">
+          <tbody>
+            <tr>
+              <td
+                className="py-5 px-8 bg-neutral-700 rounded-tl-md rounded-bl-md"
+                width="50%"
+              >
+                Tipo de transação
+              </td>
+              <td
+                className={classNames('py-5 px-8 bg-neutral-700', {
+                  'text-emerald-500': true,
+                  'text-red-400': false,
+                })}
+              >
+                R$ 12.200,00
+              </td>
+              <td className="py-5 px-8 bg-neutral-700">Casa</td>
+              <td className="py-5 px-8 bg-neutral-700 rounded-tr-md rounded-br-md">
+                13/04/2023
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        neutral: {
+          700: '#29292E',
+          800: '#202024',
+          900: '#121214',
+        },
+      },
       fontFamily: {
         sans: ['Roboto', ...defaultTheme.fontFamily.sans],
       },
